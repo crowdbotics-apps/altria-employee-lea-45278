be5501988e421d385cafe1996d167529acbdf648
Crowdbotics <team@crowdbotics.com>
2024-01-04 19:30:32 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "altria-employee-lea-45278",
+  url: "https://altria-employee-lea-45278.botics.co",
+  api: "https://altria-employee-lea-45278.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
